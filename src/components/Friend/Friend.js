26f3c21f9c66c9e79b50cb1fab52215a1729aa26
mily datta <dmily270@gmail.com>
2021-03-07 22:51:12 +0600

    finished

---
+++ src/components/Friend/Friend.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Friend = (props) => {
+    const { name, email, id } = props.friend;
+    const friendStyle = {
+        border: '1px solid purple',
+        margin: '20px',
+        padding: '20px',
+        borderRadius: '20px'
+    }
+    return (
+        <div style={friendStyle}>
+           <h2>Name: {name}</h2>
+           <p>email:{email}</p> 
+           <Link to={`/friend/${id}`}>
+            <button>show detail of {id}</button>
+            </Link>
+        </div>
+    );
+};
+
+export default Friend;
